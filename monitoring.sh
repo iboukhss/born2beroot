162ef3f3cdeab419a1625dbaa949e2a47b1ba4fb
iboukhss <151830328+iboukhss@users.noreply.github.com>
2024-05-28 00:07:09 +0200

    add ascii data

--- monitoring.sh
+++ monitoring.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 
 c1='\033[0;31m' # red
+c2='\033[0;34m' # blue
 nc='\033[0m'    # no color
+
+            read -rd '' ascii_data <<'EOF'
+       _,met$$$$$gg.
+    ,g$$$$$$$$$$$$$$$P.
+  ,g$$P"        """Y$$.".
+ ,$$P'              `$$$.
+',$$P       ,ggs.     `$$b:
+`d$$'     ,$P"'   .    $$$
+ $$P      d$'     ,    $$P
+ $$:      $$.   -    ,d$$'
+ $$;      Y$b._   _,d$P'
+ Y$$.    `.`"Y$$$$P"'
+ `$$b      "-.__
+  `Y$$
+   `Y$$.
+     `$$b.
+       `Y$$b.
+          `"Y$b._
+              `"""
+EOF
+
+echo -e "${c2}$ascii_data${nc}"
 
 # Same as `uname -m`
 echo -e "${c1}arch${nc}        $(arch)"
