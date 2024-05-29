6b5e0112dbdb0240b95da9336b0e6614c213afde
iboukhss <151830328+iboukhss@users.noreply.github.com>
2024-05-29 09:10:27 +0200

    add missing

--- monitoring.sh
+++ monitoring.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 c1='\033[0;31m' # red
-c2='\033[0;34m' # bluex
+c2='\033[0;34m' # blue
 nc='\033[0m'    # no color
 
 mapfile -t ascii <<'EOF'
@@ -31,9 +31,9 @@ arch
 kernel
 cpus
 vcpus
-mem (total)
-mem (avail)
-cpu (usage)
+ram
+disk
+cpu
 last reboot
 lvm
 connections
@@ -46,19 +46,19 @@ mapfile -t info <<EOF
 
 $(arch)
 $(uname -r)
-$(lscpu | awk '/Socket\(s\):/ {print $2}')
-$(lscpu | awk '/^CPU\(s\):/ {print $2}')
-$(free -h | awk '/^Mem:/ {print $2}')
-$(free -h | awk '/^Mem:/ {print $7}')
-
-$(who -b | awk '{print $3,$4}')
-$()
-$()
-$()
-$()
-$()
+$(lscpu | grep "Socket(s):" | awk '{print $2}')
+$(lscpu | grep "^CPU(s):" | awk '{print $2}')
+$(free -h | grep ^Mem: | awk '{printf "%s / %s (%.f%%)", $3, $2, ($3/$2)*100}')
+$(df -h --total | grep ^total | awk '{printf "%s / %s (%s)", $3, $2, $5}')
+$(top -bn1 | grep "Cpu(s)" | awk '{print $2+$4 "%"}')
+$(who -b | awk '{print $3, $4}')
+$(lvdisplay | wc -l)
+$(netstat -a | grep ESTABLISHED | wc -l)
+$(who -u | wc -l)
+$(hostname -I | awk '{printf "%s (%s)", $1, $2}')
+$(cat /var/log/secure | grep USER=root | wc -l)
 EOF
 
 for i in {0..18}; do
-	printf "${c2}%-27s${nc} ${c1}%-11s${nc} %s\n" "${ascii[i]}" "${labels[i]}" "${info[i]}"
+	printf "${c1}%-30s${nc} ${c2}%-12s${nc} %s\n" "${ascii[i]}" "${labels[i]}" "${info[i]}"
 done
